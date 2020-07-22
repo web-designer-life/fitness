98d8601181388654c101cdc505efd8695c65541a
Anton Yulbarysau <io57196657@gmail.com>
2020-07-22 21:32:53 +0300

    add: animation for arrowTop

--- src/modules/anchors.js
+++ src/modules/anchors.js
@@ -2,6 +2,33 @@ const anchorsBody = () => {
     const anchors = document.querySelectorAll('ul>li>a[href*="#"]'),
         arrowTop = document.getElementById('totop');
 
+    window.addEventListener('scroll', () => {
+        const html = document.documentElement,
+            display = window.getComputedStyle(arrowTop).display;
+
+        const fadeIn = el => {
+            if (display === 'none') {
+                let opacity = 0.01;
+                el.style.opacity = opacity;
+                el.style.display = 'block';
+                const timer = setInterval(() => {
+                    if (opacity >= 1) {
+                        clearInterval(timer);
+                    }
+                    el.style.opacity = opacity;
+                    opacity += opacity;
+                }, 30);
+            }
+        };
+
+        if (html.scrollTop > 0) {
+            console.log(display);
+            fadeIn(arrowTop);
+        } else {
+            arrowTop.style.display = 'none';
+        }
+    });
+
     arrowTop.addEventListener('click', e => {
         e.preventDefault();
         document.querySelector('body').scrollIntoView({
